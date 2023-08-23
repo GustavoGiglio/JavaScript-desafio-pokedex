eb27f50ef9459fac5a28a902a2dae8410610f194
GustavoGiglio <gtsgiglio@gmail.com>
2023-08-23 17:00:35 -0300

    enchancement(main.js/poke-api.js): Melhorando as informações que aparecem do pokemon

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,18 @@
+const pokeApi = {};
 
-const pokeApi = {}
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error))
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests)) 
+    .then((pokemonsDetails) => pokemonsDetails) 
+      
 }
+
+
