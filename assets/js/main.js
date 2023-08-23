178dd06424734fa5780ebfefa69ad75b56a0b060
GustavoGiglio <gtsgiglio@gmail.com>
2023-08-23 19:19:50 -0300

    feat(index.html/main.js): adicionado botão de paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,30 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5
+let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-  return `
-    <li class="pokemon ${pokemon.type}">
-      <span class="number">#${pokemon.number}</span>
-      <span class="name">${pokemon.name}</span>
-      <div class="detail">
-        <ol class="types">
-          ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-        </ol>
-        <img src="${pokemon.photo}" alt="B${pokemon.name}"/>
-      </div>
-    </li>
-  `
+function loadPokemonItems(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon) =>  `
+        <li class="pokemon ${pokemon.type}">
+          <span class="number">#${pokemon.number}</span>
+          <span class="name">${pokemon.name}</span>
+          <div class="detail">
+            <ol class="types">
+              ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <img src="${pokemon.photo}" alt="B${pokemon.name}"/>
+          </div>
+        </li>
+      `).join('')
+      pokemonList.innerHTML += newHtml  
+    })
+  
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItems(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('') 
+loadMoreButton.addEventListener('click', () => {
+  offset += limit
+  loadPokemonItems(offset, limit)
 })
