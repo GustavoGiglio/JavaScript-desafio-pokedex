9ae767d07b6280d7a6d5d4accd1a621915b17bca
GustavoGiglio <gtsgiglio@gmail.com>
2023-08-23 21:48:07 -0300

    docs(main.js/poke-api.js): adicionado comentários no código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,37 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonList = document.getElementById('pokemonList')        // Obtém a referência ao elemento da lista de Pokémon no HTML.
+const loadMoreButton = document.getElementById('loadMoreButton')  // Obtém a referência ao botão "Load More" no HTML.
+const maxRecords = 151                                            // Número máximo de registros de Pokémon a serem carregados.
+const limit = 10                                                  // Quantidade de Pokémon a serem carregados por vez.
+let offset = 0;                                                   // Inicializa o offset (deslocamento) para 0.
 
-const maxRecords = 151
-const limit = 10
-let offset = 0;
-
-function loadPokemonItens(offset, limit) {
-  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-    const newHtml = pokemons.map((pokemon) =>  `
-        <li class="pokemon ${pokemon.type}">
-          <span class="number">#${pokemon.number}</span>
-          <span class="name">${pokemon.name}</span>
-          <div class="detail">
-            <ol class="types">
-              ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-            </ol>
-            <img src="${pokemon.photo}" alt="B${pokemon.name}"/>
-          </div>
-        </li>
-      `).join('')
-      pokemonList.innerHTML += newHtml  
-    })
+function loadPokemonItens(offset, limit) {                        // Função para carregar itens de Pokémon com base no offset e limit especificados.
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {    // Chama o método getPokemons da pokeApi, que retorna uma promessa.
+    // Mapeia os pokémons retornados para uma string HTML representando cada um.
+    const newHtml = pokemons.map((pokemon) =>  ` 
+      <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+          <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+          </ol>
+          <img src="${pokemon.photo}" alt="B${pokemon.name}"/>
+        </div>
+      </li>
+    `).join('')
+      pokemonList.innerHTML += newHtml                            // Adiciona o HTML gerado à lista de Pokémon.
+  })
 }
 
-loadPokemonItens(offset, limit)
-
-loadMoreButton.addEventListener('click', () => {
-  offset += limit
-  const qtdRecordsWithNexPage = offset + limit
-  if (qtdRecordsWithNexPage >= maxRecords) {
-    const newLimit = maxRecords - offset
-    loadPokemonItens(offset, newLimit)
-    loadMoreButton.parentElement.removeChild(loadMoreButton)
+loadPokemonItens(offset, limit)                                   // Carrega os primeiros itens de Pokémon ao carregar a página.
+loadMoreButton.addEventListener('click', () => {                  // Adiciona um ouvinte de evento para o botão "Load More".
+  offset += limit                                                 // Incrementa o offset para a próxima página de Pokémon.
+  const qtdRecordsWithNexPage = offset + limit                    // Calcula a quantidade de registros com a próxima página.
+  if (qtdRecordsWithNexPage >= maxRecords) {                      // Verifica se a próxima página ultrapassaria o número máximo de registros.
+    const newLimit = maxRecords - offset                          // Se sim, ajusta o limite para os últimos registros.
+    loadPokemonItens(offset, newLimit)               
+    loadMoreButton.parentElement.removeChild(loadMoreButton)      // Remove o botão "Load More" quando todos os registros foram carregados.
   } else {
-    loadPokemonItens(offset, limit)
+    loadPokemonItens(offset, limit)                               // Caso contrário, carrega mais Pokémon com o limite especificado.
   }
 })
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,32 +1,34 @@
-const pokeApi = {};
+const pokeApi = {}; // Cria um objeto vazio chamado pokeApi para armazenar as funções e métodos relacionados à API de Pokémon.
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
-  const pokemon = new Pokemon();
-  pokemon.number = pokeDetail.id;
-  pokemon.name = pokeDetail.name;
+function convertPokeApiDetailToPokemon(pokeDetail) {    // Função que converte os detalhes da API de Pokémon em um objeto Pokémon personalizado.
+  const pokemon = new Pokemon();                        // Cria uma nova instância da classe Pokemon.
+  
+  // Atribui informações do detalhe do Pokémon ao objeto Pokemon.
+  pokemon.number = pokeDetail.id;   // Número do Pokémon.
+  pokemon.name = pokeDetail.name;   // Nome do Pokémon.
 
-  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
-  const [type] = types;
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);  // Mapeia os tipos do Pokémon a partir dos dados do detalhe.
+  const [type] = types;                                                  // Pega o primeiro tipo para uso posterior.
 
-  pokemon.types = types;
-  pokemon.type = type;
-  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+  pokemon.types = types;                                                  // Array de tipos do Pokémon.
+  pokemon.type = type;                                                   // Tipo principal do Pokémon.
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;    // URL da imagem do Pokémon.
 
-  return pokemon;
+  return pokemon;   // Retorna o objeto Pokémon personalizado.
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
-  return fetch(pokemon.url)
-    .then((response) => response.json())
-    .then(convertPokeApiDetailToPokemon);
+pokeApi.getPokemonDetail = (pokemon) => {   // Método para obter detalhes de um Pokémon específico.
+  return fetch(pokemon.url)                 // Faz uma solicitação à API usando a URL do Pokémon.
+    .then((response) => response.json())    // Converte a resposta em formato JSON.
+    .then(convertPokeApiDetailToPokemon);   // Converte os detalhes em um objeto Pokémon personalizado.
 };
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
-  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-  return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-    .then((detailRequests) => Promise.all(detailRequests))
-    .then((pokemonsDetails) => pokemonsDetails);
+pokeApi.getPokemons = (offset = 0, limit = 10) => {                                 // Método para obter uma lista de Pokémons com base no deslocamento e limite.
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;  // Monta a URL da API.
+  return fetch(url)                                                                 // Faz uma solicitação à API usando a URL montada.
+    .then((response) => response.json())                                            // Converte a resposta em formato JSON.
+    .then((jsonBody) => jsonBody.results)                                           // Pega a matriz de resultados da resposta.
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))                     // Mapeia os detalhes de cada Pokémon.
+    .then((detailRequests) => Promise.all(detailRequests))                          // Faz todas as solicitações de detalhes em paralelo.
+    .then((pokemonsDetails) => pokemonsDetails);                                    // Retorna a lista de detalhes de Pokémon.
 };
